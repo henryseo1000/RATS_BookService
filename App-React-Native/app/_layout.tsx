6282a903123299605d50e5a60c3bfff963ac8c9e
henryseo1000 <henryseo1000@gmail.com>
2024-07-06 20:19:39 +0900

    link with convex - App

--- App-React-Native/app/_layout.tsx
+++ App-React-Native/app/_layout.tsx
@@ -2,6 +2,7 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
@@ -9,6 +10,9 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+});
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -28,10 +32,12 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      <ConvexProvider client={convex}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </ConvexProvider>
     </ThemeProvider>
   );
 }
