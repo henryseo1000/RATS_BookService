02b36e66b6cd7413233fe01374dbf090a1c88f3f
서호준 <henryseo1000@gmail.com>
2026-03-20 02:34:37 +0900

    불필요한 xml 함수 제거

--- web-next-js/src/utils/xmlToJson.ts
+++
@@ -1,9 +0,0 @@
-export const xmlToJson = (xmlString : string) => {
-    const jsonData = {};
-    for (const result of xmlString.matchAll(/(?:<(\w*)(?:\s[^>]*)*>)((?:(?!<\1).)*)(?:<\/\1>)|<(\w*)(?:\s*)*\/>/gm)) {
-        const key = result[1] || result[3];
-        const value = result[2] && xmlToJson(result[2]);
-        jsonData[key] = ((value && Object.keys(value).length) ? value : result[2]) || null;
-   }
-   return jsonData;
- }
